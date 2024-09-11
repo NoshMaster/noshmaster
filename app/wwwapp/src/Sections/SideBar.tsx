623359593cb37864de642466b9183ab6e12d7976
yllaevv <yllanselme@gmail.com>
2024-09-11 23:41:14 +0200

    Init: Change logo & metadata

--- app/wwwapp/src/Sections/SideBar.tsx
+++ app/wwwapp/src/Sections/SideBar.tsx
@@ -2,23 +2,19 @@ import { Link } from "react-router-dom"
 
 export default function SideBar() {
     return (
-        <div>
-            <div style={{minWidth: "100%", display: "table"}}>
-                <div className="w-full">
-                    <div className="pb-4">
-                        <h4 className="mb-1 rounded-md px-2 py-1 text-sm font-semibold">Get Started</h4>
-                        <div className="grid grid-flow-row auto-rows-max text-sm">
-                            {/* <Link to="#" className="group flex w-full items-center rounded-md border border-transparent px-2 py-1 hover:underline text-muted-foreground">Introduction</Link> */}
-                            <Link to="/docs/installation" className="group flex w-full items-center rounded-md border border-transparent px-2 py-1 hover:underline text-muted-foreground">Installation</Link>
-                        </div>
-                    </div>
-                    <div className="pb-4">
-                        <h4 className="mb-1 rounded-md px-2 py-1 text-sm font-semibold">Animations</h4>
-                        <div className="grid grid-flow-row auto-rows-max text-sm">
-                            <Link to="/docs/animation/fade-in" className="group flex w-full items-center rounded-md border border-transparent px-2 py-1 hover:underline text-muted-foreground">Fade-In</Link>
-                            <Link to="/docs/animation/perspective" className="group flex w-full items-center rounded-md border border-transparent px-2 py-1 hover:underline text-muted-foreground">Perspective</Link>
-                        </div>
-                    </div>
+        <div className="pt-2">
+            <div className="pb-4">
+                <h4 className="mb-1 rounded-md py-1 text-sm font-semibold">Get Started</h4>
+                <div className="grid grid-flow-row auto-rows-max text-sm">
+                    {/* <Link to="#" className="group flex w-full items-center rounded-md border border-transparent px-2 py-1 hover:underline text-muted-foreground">Introduction</Link> */}
+                    <Link to="/docs/installation" className="group flex w-full items-center rounded-md border border-transparent px-2 py-1 hover:underline text-muted-foreground">Installation</Link>
+                </div>
+            </div>
+            <div className="pb-4">
+                <h4 className="mb-1 rounded-md py-1 text-sm font-semibold">Animations</h4>
+                <div className="grid grid-flow-row auto-rows-max text-sm">
+                    <Link to="/docs/animation/fade-in" className="group flex w-full items-center rounded-md border border-transparent px-2 py-1 hover:underline text-muted-foreground">Fade-In</Link>
+                    <Link to="/docs/animation/perspective" className="group flex w-full items-center rounded-md border border-transparent px-2 py-1 hover:underline text-muted-foreground">Perspective</Link>
                 </div>
             </div>
         </div>
